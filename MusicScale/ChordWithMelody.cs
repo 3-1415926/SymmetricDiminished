b301c9d6f8019aff72d5cf8655962d985a786aed
3-1415926 <320x200@gmail.com>
2014-02-26 22:31:36 -0800

    Adding missing files again

---
+++ MusicScale/ChordWithMelody.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicScale
+{
+    public class ChordWithMelody
+    {
+        public readonly string ChordNotation;
+        public readonly Chord Chord;
+        public readonly Note[] MelodyNotes;
+        public readonly ulong MelodyMask;
+
+        public ChordWithMelody(string chordNotation, params Note[] melodyNotes)
+        {
+            this.ChordNotation = chordNotation;
+            this.Chord = new Chord(chordNotation);
+            this.MelodyNotes = melodyNotes;
+            this.MelodyMask = 0;
+            foreach (var note in melodyNotes)
+                this.MelodyMask |= Common.NoteMaskInAllOctaves((int)note);
+        }
+    }
+}
